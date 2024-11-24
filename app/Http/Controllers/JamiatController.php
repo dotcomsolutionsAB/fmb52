61e5459453dd916173cf21ecb0e243904527f6a9
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-11-24 17:55:45 +0530

    ..

--- app/Http/Controllers/JamiatController.php
+++ app/Http/Controllers/JamiatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\JamiatModel;
+use App\Models\User;
 use App\Models\JamiatSettingsModel;
 use App\Models\SuperAdminReceiptsModel;
 use App\Models\SuperAdminCounterModel;
