f10e582c6efdd50c4c99875c9ae93c2429380591
Husain990 <husain990@gmail.com>
2024-11-27 00:49:47 +0530

    trying last

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,22 +50,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         // Authentication Middleware
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-
-        // Authorization Middleware
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-
-        // Route Middleware
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
+       
         // Sanctum Middleware
         'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 
