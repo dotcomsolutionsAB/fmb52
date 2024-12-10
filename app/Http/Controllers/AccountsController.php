903731984b2218419228b8aae84b25cec93ed298
Husain990 <husain990@gmail.com>
2024-12-10 18:55:50 +0530

    try 21

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -748,7 +748,7 @@ class AccountsController extends Controller
             'group_id' => 'receipt_' . uniqid(),
             'callback_data' => 'receipt_' . $receipt->receipt_no,
             'recipient_type' => 'individual',
-            'to' => $receipt->mobile, // Use mobile number
+            'to' => '917439515253', // Use mobile number
             'template_name' => 'fmb_receipt_created',
             'content' => json_encode($templateContent), // Encode the content as JSON
             'file_url' => $fullPdfUrl, // Attach the full PDF URL
