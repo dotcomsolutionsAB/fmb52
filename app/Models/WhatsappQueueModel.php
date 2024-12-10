369f8aea25c4cee782b725ab71dc9d6227c3d624
Husain990 <husain990@gmail.com>
2024-12-10 18:04:49 +0530

    try 10

--- app/Models/WhatsappQueueModel.php
+++ app/Models/WhatsappQueueModel.php
@@ -6,10 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class WhatsappQueueModel extends Model
 {
-    //
+    // Define the table associated with the model
     protected $table = 't_whatsapp_queue';
 
+    // Define fillable attributes for mass assignment
     protected $fillable = [
-        'jamiat_id', 'family_id', 'group_id', 'callback_url', 'to', 'template_name', 'content', 'json', 'response', 'status', 'log_user'
+        'jamiat_id', 
+        'recipient_type', 
+        'group_id', 
+        'callback_data', 
+        'to', 
+        'template_name', 
+        'content', 
+        'response', 
+        'status', 
+        'log_user'
     ];
+
+    // Define any relationships, if applicable
+    // For example, if this model is related to Jamiat or Users:
+    // public function jamiat()
+    // {
+    //     return $this->belongsTo(JamiatModel::class, 'jamiat_id');
+    // }
 }
