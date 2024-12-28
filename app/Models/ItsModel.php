35d753a0376e83348a211451c9d9a7161fd2e0b6
Husain990 <husain990@gmail.com>
2024-12-28 13:10:30 +0530

    pushh

--- app/Models/ItsModel.php
+++ app/Models/ItsModel.php
@@ -10,6 +10,6 @@ class ItsModel extends Model
     protected $table = 't_its_data';
 
     protected $fillable = [
-        'jamiat_id', 'hof_its', 'its_family_id', 'name', 'email', 'mobile', 'title', 'mumeneen_type', 'gender', 'age', 'sector', 'sub_sector', 'name_arabic'
+        'jamiat_id','its', 'hof_its', 'its_family_id', 'name', 'email', 'mobile', 'title', 'mumeneen_type', 'gender', 'age', 'sector', 'sub_sector', 'name_arabic'
     ];
 }
