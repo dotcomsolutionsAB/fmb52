514388f44c1a6527c87e91e883a760cd981e339a
Husain990 <husain990@gmail.com>
2025-05-07 22:07:37 +0530

    ..

--- routes/api.php
+++ routes/api.php
@@ -290,7 +290,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/by-date', [MenuController::class, 'getMenuByDate']); // Get menu by a specific date
         Route::post('/by-week', [MenuController::class, 'getMenuForWeek']); // Get menu for the week
         Route::post('/update/{id}', [MenuController::class, 'update_menu']); // Update
-    Route::delete('/delete/{id}', [MenuController::class, 'delete_menu']); // 
+    Route::delete('/{id}', [MenuController::class, 'delete_menu']); // 
     });
 
 });
