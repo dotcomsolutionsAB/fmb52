de4b95b860b4526e68ac88e8e6b1ba75a1ca5022
Husain990 <husain990@gmail.com>
2025-05-15 23:05:12 +0530

    ..

--- app/Http/Controllers/FeedbacksController.php
+++ app/Http/Controllers/FeedbacksController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Feedback;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class FeedbacksController extends Controller
 {
